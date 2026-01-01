8aaab28490f63e15b1f1c32aa5b90d4ed65ef672
Antarctic Flakes <antarcticflakes@outlook.com>
2026-01-01 07:04:35 +0000

    update

--- config/basic-init.el
+++ config/basic-init.el
@@ -11,11 +11,20 @@
 (add-hook 'prog-mode-hook #'show-paren-mode)
 (column-number-mode t)
 (global-auto-revert-mode t)
+
 (delete-selection-mode t)
+
 (setq inhibit-startup-message t)
 (setq auto-save-default nil)
 (setq make-backup-files nil)
+
 (global-display-line-numbers-mode 1)
+
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+
+(global-hl-line-mode t)
+
 (when (display-graphic-p)
   (menu-bar-mode -1)
   (tool-bar-mode -1))
